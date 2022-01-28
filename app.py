73b8ae2b9e66b80580aa426cb740f09852e492a0
tedoiler <ladas.thodoris@gmail.com>
2022-01-28 19:33:46 +0000

    cosmetic changes on app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 from matplotlib import pyplot as plt
 import fun_preprocessing as fp
 import fun_models as fm
@@ -10,8 +11,8 @@ def convert_df(df):
     return df.to_csv().encode('utf-8')
 
 st.set_option('deprecation.showPyplotGlobalUse', False) #Temporary
-st.title("Cashflow Forecasting")
-st.sidebar.title("Sidebar")
+st.title("Monitoring")
+st.sidebar.title("Cashflow Forecasting")
 
 # Input Form --------------------------------------------
 st.sidebar.header("Input File")
@@ -23,7 +24,7 @@ with input_expander:
         try:
             df = pd.read_csv(uploaded_file, header=[0])
         except Exception:
-            df = pd.reaad_excel(uploaded_file, header=[0])
+            df = pd.read_excel(uploaded_file, header=[0])
         try:
             data = fp.clean_data(data=df)
         except Exception:
@@ -34,12 +35,12 @@ with input_expander:
         data = pd.DataFrame()
 
 col1, col2 = input_form.columns(2)
-submit_data_btn = col1.form_submit_button("Submit Data")
+submit_data_btn = col1.form_submit_button("Submit")
 if submit_data_btn:
     col2.success("Success")
 input_data_expander = input_form.expander("Raw data preview")
 with input_data_expander:
-    input_data_expander.write(data)
+    input_data_expander.dataframe(data)
 
 # Parameters Form --------------------------------------------
 st.sidebar.header("Parameters")
@@ -53,11 +54,11 @@ with params_expander:
     live = col2.checkbox("Live")
     f_period = col1.slider("Forecast periods", 1, 12, 1)
     confidence_int = col1.slider("Confidence Int", 0., 1., 0.05)
-    search_strength = col2.slider("Search Strength", 1, 7, 1)
+    search_strength = col2.slider("Search Strength", 1, 10, 1)
     dummy = col2.slider("Dummy", 1, 7, 1)
 
 col1, col2 = parameter_form.columns(2)
-submit_params_btn = col1.form_submit_button("Submit Parameters")
+submit_params_btn = col1.form_submit_button("Submit")
 try:
     group = fp.group_data(data=data, grouping=grouping, log_bool=log_transform)
     if submit_params_btn:
@@ -69,18 +70,19 @@ except Exception:
 group_expander = parameter_form.expander("Grouped data preview")
 
 with group_expander:
-    group_expander.write(group)
+    group_expander.dataframe(group)
 
 # Model ---------------------------------------------------
 model_expader = st.expander("Model")
 if submit_params_btn:
-    train, val, model, pred, results = fm.run_arima_routine_st(data=group,
-                                                               grouping=grouping,
-                                                               forecast_periods=f_period,
-                                                               confidence_int=confidence_int,
-                                                               periods_search=search_strength,
-                                                               log_bool=log_transform,
-                                                               live=live)
+    with st.spinner('Training Model...'):
+        train, val, model, pred, results = fm.run_arima_routine_st(data=group,
+                                                                   grouping=grouping,
+                                                                   forecast_periods=f_period,
+                                                                   confidence_int=confidence_int,
+                                                                   periods_search=search_strength,
+                                                                   log_bool=log_transform,
+                                                                   live=live)
 
     model_expader.success('Model Trained')
     csv = convert_df(results)
@@ -88,11 +90,21 @@ if submit_params_btn:
     model_expader.download_button(label="Download data as CSV", data=csv, file_name='output.csv', mime='text/csv',)
 
     # Plotting results
-    true = pd.concat([train, val], axis=0)
-    true_mod = pd.concat([true, model.predict()], axis=1)
-    all = pd.concat([true_mod, pred], axis=0)
+    true = pd.concat([round(np.exp(train),2), round(np.exp(val),2)], axis=0)
+    true_mod = pd.concat([true, round(np.exp(model.predict()),2)], axis=1)
+    all = pd.concat([true_mod, round(np.exp(pred),2)], axis=0)
     all.columns = ['true', 'predicted', 'forecast']
     all.plot()
     model_expader.pyplot()
 else:
-    model_expader.warning("Input from sidebar expected")
+    model_expader.info("Input from sidebar expected")
+
+
+# Documentation --------------------------------------------
+st.sidebar.header("Documentation")
+document_expander = st.sidebar.expander("Search Documentation")
+with document_expander:
+    code_link = '[Source code](https://github.com/TedOiler/forecasting-app)'
+    document_expander.markdown(code_link, unsafe_allow_html=True)
+    documentation_link = '[Documentation](https://github.com/TedOiler/forecasting-app)'
+    document_expander.markdown(documentation_link, unsafe_allow_html=True)
